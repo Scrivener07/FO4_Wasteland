b11ba077a18bde5a1f0849f0aac9292f8b00d5ea
Scrivener07 <ScrivenerMods@gmail.com>
2017-01-28 22:44:49 -0800

    adding project files

---
+++ Mods/UserWasteland/Overlays/Command_Papyrus.bat
@@ -0,0 +1,16 @@
+@ECHO off
+ECHO Setting batch variables..
+
+REM User
+REM ===========================================================
+SET falloutDirectory=D:\Games\Steam\SteamApps\common\Fallout 4\
+
+REM Location Paths
+REM ===========================================================
+SET ppj01=Mods\UserWasteland\Overlays\Overlays.ppj
+SET compilePPJ="%falloutDirectory%%ppj01%"
+
+REM Start the papyrus compiler
+REM ===========================================================
+ECHO Compiling papyrus with %compilePPJ%
+"%falloutDirectory%Papyrus compiler\PapyrusCompiler.exe" %compilePPJ%
